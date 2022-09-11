d04aae2ae4afcc845f7b0afd50783b0b723903e2
MaryamNozari <103874176+MaryamNozari@users.noreply.github.com>
2022-09-11 10:58:10 +0430

    adding form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import shecodeslogo from "./shecodeslogo.png";
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={shecodeslogo} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header"></header>
+        <img src={shecodeslogo} className="App-logo img-fluid" alt="logo" />
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Maryam Nozari</footer>
+      </div>
     </div>
   );
 }
