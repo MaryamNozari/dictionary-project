6140c9ba128edb654c20786fa894cfd368932ae0
MaryamNozari <103874176+MaryamNozari@users.noreply.github.com>
2022-09-11 09:52:19 +0430

    initial version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import shecodeslogo from "./shecodeslogo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={shecodeslogo} className="App-logo img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
