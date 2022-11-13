5f48b1022c395205ec1228f5f6fcb61f61f51cf3
MaryamNozari <103874176+MaryamNozari@users.noreply.github.com>
2022-11-13 21:44:56 +0330

    adding synonyms

--- src/Result.js
+++ src/Result.js
@@ -2,6 +2,7 @@ import React from "react";
 import Meaning from "./Meaning.js";
 
 export default function Result(props) {
+  console.log(props.result)
   if (props.result) {
     return (
       <div className="Result">
